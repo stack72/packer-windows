a878c13ce0b68349bee4687b78c486779b9b7c18
stack72 <public@paulstack.co.uk>
2013-10-14 22:52:36 +0100

    Changing the packer for windows2008 to use puppet OSS as well as removing the VBox provisioner from it

--- scripts/install-puppet.sh
+++ scripts/install-puppet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-wget --no-check-certificate http://pm.puppetlabs.com/puppet-enterprise/3.0.1/puppet-enterprise-3.0.1.msi -O puppet.msi
+wget --no-check-certificate https://downloads.puppetlabs.com/windows/puppet-3.3.1.msi -O puppet.msi
 msiexec /qn /i puppet.msi
 rm -rf puppet.msi
 cmd.exe /c setx /m PATH "%path%;C:\\Program Files (x86)\\Puppet Labs\\Puppet Enterprise\\bin"
